d7246f3e7573e8e85779db95e4bee11b527c6952
motthoicodon <hoang.minh@vmodev.com>
2019-02-28 14:08:09 +0700

    Create Database using Migrations

---
+++ database/migrations/2019_02_28_064657_create_members_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',50);
+            $table->string('information',300)->nullable();
+            $table->string('phone',20);
+            $table->date('birthday');
+            $table->string('avartar')->nullable();
+            $table->string('position');
+            $table->string('gender');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
---
+++ database/migrations/2019_02_28_070233_create_works_on_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWorksOnTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('works_on', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('member_id')->unsigned();
+            $table->integer('project_id')->unsigned();
+            $table->string('role');
+
+            $table->foreign('member_id')->references('id')->on('members');
+            $table->foreign('project_id')->references('id')->on('projects');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('works_on');
+    }
+}
