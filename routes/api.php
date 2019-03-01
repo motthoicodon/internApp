b7a44739c0cdf237aba774cb0dcfb94ee88cad30
motthoicodon <hoang.minh@vmodev.com>
2019-03-01 10:08:19 +0700

    Prepare for Back-end

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('members', 'MemberController',
+                            ['except' => ['create','edit']]);
+
+Route::resource('projects', 'ProjectController',
+                            ['except' => ['create','edit']]);
