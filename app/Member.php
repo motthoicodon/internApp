5c1601c113daaa656831b0f870899417e10640d2
motthoicodon <hoang.minh@vmodev.com>
2019-03-07 15:43:11 +0700

    Implementing Models

--- app/Member.php
+++ app/Member.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Member extends Model
 {
     use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'information',
+        'phone',
+        'birthday',
+        'avatar',
+        'position',
+        'gender',
+    ];
+
+    public function projects()
+    {
+        return $this->belongsToMany('App\Project', 'works_on');
+    }
 }
--- app/Project.php
+++ app/Project.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Project extends Model
 {
     use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'information',
+        'deadline',
+        'type',
+        'status'
+    ];
+
+    public function members()
+    {
+        return $this->belongsToMany('App\Member', 'works_on');
+    }
 }
