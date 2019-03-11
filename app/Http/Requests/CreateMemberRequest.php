539c2d9854b1cc9b0768f0ab7d79112f71cf6c1b
motthoicodon <hoang.minh@vmodev.com>
2019-03-11 12:11:57 +0700

    Implementing Index - Show - Store Method of MemberController

---
+++ app/Http/Requests/CreateMemberRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Member;
+use App\Rules\DateOfBirth;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateMemberRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required','max:50','regex:/^[a-zA-Z0-9-.\s]*$/'],
+            'information' => 'max:300',
+            'phone' => ['required','max:20','regex:/^[0-9()-.+\s\/]*$/'],
+            'birthday' => ['required','date_format:"Y/m/d"','before:today', new DateOfBirth()],
+            'avatar' => 'file|image|max:10240',
+            'position'      => 'required|in:' . implode(',', Member::POSITIONS),
+            'gender'      => 'required|in:' . implode(',', Member::GENDERS),
+        ];
+    }
+
+}
--- app/Member.php
+++ app/Member.php
@@ -4,10 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Http\Request;
 
 class Member extends Model
 {
     use SoftDeletes;
+
+    const POSITIONS = ['intern','junior','senior','pm','ceo','cto','bo'];
+    const GENDERS = ['male','female'];
 
     protected $fillable = [
         'name',
@@ -23,4 +27,37 @@ class Member extends Model
     {
         return $this->belongsToMany('App\Project', 'works_on');
     }
+
+    public function workson()
+    {
+        return $this->hasMany(WorksOn::class);
+    }
+
+    public function getAll()
+    {
+        return self::all();
+    }
+
+    public function find(int $id)
+    {
+        return self::findOrFail($id);
+    }
+
+    public function store(Request $request){
+        $input = $request->only([
+            'name',
+            'information',
+            'phone',
+            'birthday',
+            'avatar',
+            'position',
+            'gender'
+        ]);
+
+        if($request->avatar != null){
+            $input['avatar'] = $request->avatar->store('');
+        }
+        return self::create($input);
+    }
+
 }
