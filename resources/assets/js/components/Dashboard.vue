c547d6b365fcff9d8a50e7c461011512b2a91933
motthoicodon <hoang.minh@vmodev.com>
2019-03-21 10:22:52 +0700

    Setting up Router and Creating Component for Frontend

---
+++ resources/assets/js/components/Dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Dasboard</h1>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+    export default {
+
+    }
+</script>
+
+<style scoped>
+    
+</style>
---
+++ resources/assets/js/components/Project.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Projects</h1>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+    export default {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
